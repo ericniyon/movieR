986514f853d0d93e0fff99610478a6d499feb531
eric <niyo1eric@gmail.com>
2019-02-23 18:23:29 +0200

    console application

---
+++ Cons.Db/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cons.Db
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movietbl> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=BLESSED-ONE\MADIBASQL;Database=EFGetStarted.ConsoleApp.NewDb;Trusted_Connection=True;");
+        }
+    }
+
+    public class Movietbl
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string title { get; set; }
+        public string year { get; set; }
+        public string category { get; set; }
+        public int budget { get; set; }
+
+    }
+}
---
+++ Cons.Db/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cons.Db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieContext())
+            {
+                Console.WriteLine("enter movie title");
+                string title = Console.ReadLine();
+                Console.WriteLine("enter movie realised year");
+                string year = Console.ReadLine();
+                Console.WriteLine("enter movie category_name");
+                string category = Console.ReadLine();
+                Console.WriteLine("enter movie budget");
+                int budget = Convert.ToInt32(Console.ReadLine());
+                db.Movies.Add(new Movietbl { title = title });
+                db.Movies.Add(new Movietbl { year = year });
+                db.Movies.Add(new Movietbl { category = category });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All movies in database:");
+                foreach (var movietbl in db.Movies)
+                {
+                    Console.WriteLine(" - {0} {1}{2}{3}", movietbl.title, movietbl.year, movietbl.category, movietbl.budget);
+                }
+            }
+        }
+    }
+    }
+
